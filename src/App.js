e382c549a079d7d2c86993e737f3badfd13f4237
Tiago 3 <tiago-tmt@hotmail.com>
2022-10-18 11:34:26 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import "./style.css"
+
+export default function App() {
+    // declaração State
+    // useState()
+
+    // pegando array e a função(por ela que o array poderá ser modificado) retornada por useState
+    // const result = React.useState("Yes")
+
+    // O valor definido entre parenteses, será o valor padrao do State
+    // destructuring array(pegando o que for utilizar)
+    const [isImportant, setIsImportant] = React.useState("Yes")
+
+    // Definindo novo valor para isImportant através da função
+    // setIsImportant("No")
+
+    function handleClick() {
+
+        setIsImportant("No")
+
+    }
+
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value" onClick={handleClick}>
+                {/* <h1>Yes</h1> */}
+                <h1>{isImportant}</h1>
+            </div>
+        </div>
+    )
+}
